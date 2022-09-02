c275651c7a5fbc64a84d43c3d9c7b99f3488535f
Gurkirpal Singh <12171804+gpalsingh@users.noreply.github.com>
2022-09-02 18:32:02 +0530

    Initial commit

---
+++ src/FileUpload.js
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import CircularProgress from '@mui/material/CircularProgress';
+const axios = require('axios').default;
+
+export default function FileUpload() {
+
+  const [fileName, setFileName] = React.useState('');
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const onFileChange = (event) => {
+    const pdfFile = event?.target?.files?.[0];
+
+    if (!pdfFile) {
+      console.error('Invalid file');
+      return;
+    }
+
+    console.log(pdfFile);
+
+    setFileName(pdfFile.name);
+    requestUploadUrl(pdfFile);
+  };
+
+  const uploadFile = (pdfFile, uploadUrl) => {
+    const options = {
+      headers: {
+        'Content-Type': pdfFile.type
+      }
+    };
+
+    console.log({ uploadUrl, pdfFile, options });
+    axios.put(uploadUrl, pdfFile, options).finally(() => {
+      setIsLoading(false);
+    });
+  }
+
+  const requestUploadUrl = (pdfFile) => {
+    setIsLoading(true);
+    axios.get('https://7o2zbtjo2c.execute-api.ap-south-1.amazonaws.com/uploads')
+      .then(function (response) {
+        // handle success
+        const { Key: newFileName, uploadURL } = response.data;
+        console.log(response);
+        uploadFile(pdfFile, uploadURL);
+      })
+      .catch(function (error) {
+        // handle error
+        setIsLoading(false);
+        console.log(error);
+      });
+  }
+
+  return (
+    <div>
+      {fileName && <div>{isLoading && 'Uploading:'} {fileName}</div>}
+      <Stack direction="row" alignItems="center" spacing={2}>
+        <Button variant="contained" component="label" disabled={isLoading}>
+          Upload
+          <input hidden accept="application/pdf" type="file" onChange={onFileChange} />
+        </Button>
+        {isLoading && <CircularProgress disableShrink />}
+      </Stack></div>
+  );
+}
